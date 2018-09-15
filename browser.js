184de5e46eb341cf117ef0277084069f6d973b34
fluidbyte <kent@fluidbyte.net>
2018-09-15 16:25:49 +0000

    feat(browser): add bundling for browser

---
+++ browser.js
@@ -0,0 +1,14 @@
+import isObj from './lib/isObj'
+import keys from './lib/keys'
+import toPairs from './lib/toPairs'
+import fromPairs from './lib/fromPairs'
+import get from './lib/get'
+import set from './lib/set'
+import flatten from './lib/flatten'
+import expand from './lib/expand'
+import redact from './lib/redact'
+import merge from './lib/merge'
+
+export {
+  isObj, keys, toPairs, fromPairs, get, set, flatten, expand, redact, merge
+}
